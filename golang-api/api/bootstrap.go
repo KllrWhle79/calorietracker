40e159abed1cffd0eab3322a0f7c8f377076cbc3
Brent Creager <KllrWhle79@hotmail.com>
2021-11-24 14:11:04 -0700

    Bootstrapping login Call

--- golang-api/api/bootstrap.go
+++ golang-api/api/bootstrap.go
@@ -26,16 +26,22 @@ func makeRouter() *mux.Router {
 
 	router.HandleFunc("/", RootHandler)
 	router.HandleFunc("/ping", PingHandler)
+	router.HandleFunc("/login", LoginHandler).Methods("POST")
 
 	router.HandleFunc("/user", CreateUser).Methods("PUT")
 	router.HandleFunc("/user", GetUser).Queries("id", "{id}").Methods("GET")
 	router.HandleFunc("/user", DeleteUser).Queries("id", "{id}").Methods("DELETE")
 	router.HandleFunc("/user", UpdateUser).Queries("id", "{id}").Methods("POST")
 
+	router.HandleFunc("/calories", CreateCalorieEntry).Methods("PUT")
+	router.HandleFunc("/calories", GetCaloriesForUser).Queries("id", "{id}").Methods("GET")
+	router.HandleFunc("/calories", DeleteCalorieEntry).Queries("id", "{id}").Methods("DELETE")
+	router.HandleFunc("/calories", UpdateCalorieEntry).Queries("id", "{id}").Methods("POST")
+
 	return router
 }
 
-// swagger:operation GET / root
+// swagger:operation GET / root mainPage
 // ---
 // summary: Return a message for the root call
 // description: Will return a canned message when the root URL is hit
@@ -46,7 +52,7 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-// swagger:operation GET /ping ping
+// swagger:operation GET /ping root pingPong
 // ---
 // summary: Return a message for the ping call
 // description: Returns a pong signifying the server is up
@@ -55,4 +61,15 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 func PingHandler(w http.ResponseWriter, r *http.Request) {
 	response := map[string]string{"message": "pong ping pong"}
 	json.NewEncoder(w).Encode(response)
+}
+
+// swagger:operation POST /login root loginUser
+// ---
+// summary: Login user
+// description: "Logs in a user based on password and loads applicable calorie information. Loads admin console for admins"
+// responses:
+//  "200": "User logged in, JWT token created"
+//  "401": "Bad username or password"
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
 }
